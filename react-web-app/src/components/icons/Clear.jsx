cc46086f0986c106c55b3e57016c23c2ce114467
Väinö Järvelä <vaino@speechly.com>
2019-11-01 09:51:00 +0200

    Fix case of DOM attribute

--- react-web-app/src/components/icons/Clear.jsx
+++ react-web-app/src/components/icons/Clear.jsx
@@ -24,7 +24,7 @@ export const Clear = ({ onClear, disabled }) => {
       <svg
         xmlns="http://www.w3.org/2000/svg"
         aria-hidden="true"
-        tabindex="0"
+        tabIndex="0"
         viewBox="0 0 20 20"
       >
         <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
