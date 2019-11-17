12c9ae372d44db887f9089d6dd5a560140f4dc58
Atte Gartman <atte.gartman@gmail.com>
2019-11-17 23:15:06 +0200

    Recording indicator to header and logo

--- react-web-app/src/Header.jsx
+++ react-web-app/src/Header.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import AppContext from './context/AppContext'
-
+import { RecordingWave } from './components/icons/RecordingWave'
+import Logo from './SuperValu_Logo.jpg'
 export default function Header() {
   return (
     <AppContext.Consumer>
-      {() => <header></header>}
+      {({ sluState }) => (
+        <header className="header">
+          <div>
+            <img
+              src={Logo}
+              alt=""
+              className={`header__logo ${
+                sluState !== 'Recording' ? 'header__logo--active' : ''
+              }`}
+            />
+            <RecordingWave
+              className={`header__logo ${
+                sluState === 'Recording' ? 'header__logo--active' : ''
+              }`}
+            />
+          </div>
+          <div></div>
+        </header>
+      )}
     </AppContext.Consumer>
   )
 }
