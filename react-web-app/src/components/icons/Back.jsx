d008c1202696e596739c8546ad8d5c262afb5dd3
Atte Gartman <atte.gartman@gmail.com>
2019-11-22 15:02:09 +0200

    Focus on first item when recording, minor fixes

--- react-web-app/src/components/icons/Back.jsx
+++ react-web-app/src/components/icons/Back.jsx
@@ -10,20 +10,16 @@ export const Back = ({ onBack, disabled }) => {
     }
   )
   return (
-    <button
-      className={className}
-      disabled={disabled}
-      onClick={onBack}
-    >
+    <div className={className} disabled={disabled} onClick={onBack}>
       <object>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-        <polygon
-          points="3.828 9 9.899 2.929 8.485 1.515 0 10 .707 10.707 8.485 18.485 9.899 17.071 3.828 11 20 11 20 9 3.828 9"
-          aria-hidden="true"
-          tabIndex="0"
-        />
-      </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+          <polygon
+            points="3.828 9 9.899 2.929 8.485 1.515 0 10 .707 10.707 8.485 18.485 9.899 17.071 3.828 11 20 11 20 9 3.828 9"
+            aria-hidden="true"
+            tabIndex="0"
+          />
+        </svg>
       </object>
-    </button>
+    </div>
   )
 }
